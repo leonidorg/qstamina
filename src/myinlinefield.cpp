427f2910168f090f73aaccfe4d652b75b82d4108
leonidorg <leonidorg@gmail.com>
2015-03-28 18:58:53 +0600

    обеспечивает правильное отображение скорости при безостановочном вводе непрерывного текста.

--- src/myinlinefield.cpp
+++ src/myinlinefield.cpp
@@ -1,6 +1,9 @@
 #include "myinlinefield.h"
+#include "textfield.h"
 
-MyInlineField::MyInlineField()
+
+MyInlineField::MyInlineField(Sounds *sounds, QWidget *parent)  :
+    InlineField(sounds, parent)
 {
 
 }
@@ -10,3 +13,27 @@ MyInlineField::~MyInlineField()
 
 }
 
+ QString MyInlineField::lastSymbol()
+ {
+    if(!m_oldText->text().isEmpty())
+        return m_oldText->text().at(m_oldText->text().size()-1);
+    else
+        return " ";
+ }
+ void MyInlineField::start()
+ {
+     m_rightSymbols = 0;
+     m_wrongSymbols = 0;
+ }
+
+ void MyInlineField::stop()
+ {
+
+ }
+
+ void MyInlineField::reset()
+ {
+     m_oldText->setText(m_oldText->text().right(40));
+     m_newText->setText(m_text);
+     m_countSymbols = m_text.size();
+ }
