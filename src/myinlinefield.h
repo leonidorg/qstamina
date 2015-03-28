751e6b62c6ae334cae15073038a02e461550d90a
leonidorg <leonidorg@gmail.com>
2015-03-28 18:56:05 +0600

    Перекрываем важные методы, это позволит синхронизировать  таймер с непреывным вводом текста.

--- src/myinlinefield.h
+++ src/myinlinefield.h
@@ -1,12 +1,16 @@
 #ifndef MYINLINEFIELD_H
 #define MYINLINEFIELD_H
-
-
+# include <QString>
+#include "inlinefield.h"
 class MyInlineField : public InlineField
 {
 public:
-    MyInlineField();
+    MyInlineField(Sounds *sounds, QWidget *parent = 0);
     ~MyInlineField();
+    QString lastSymbol();
+    void reset();
+    void start();
+    void stop();
 };
 
 #endif // MYINLINEFIELD_H
